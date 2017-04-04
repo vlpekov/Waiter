1d81807d08aeae57df9b3a4068572d7b18efeafc
vlpekov <vladdey@gmail.com>
2017-04-04 16:22:24 +0300

    New class: MenuItem

---
+++ src/MenuItem.java
@@ -0,0 +1,31 @@
+
+public class MenuItem {
+	private String name;
+	private double price;
+	private String category;
+	public MenuItem(String name, double price, String category) {
+		this.name = name;
+		this.price = price;
+		this.category = category;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+}
