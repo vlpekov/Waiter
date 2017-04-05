7a258ddbf11a325539eea3f3c850dd9c02f98128
vlpekov <vladdey@gmail.com>
2017-04-05 09:43:33 +0300

    New class: MenuItemTable New packeges: "GUI", "RestaurantObjects" Added new field: MenuItem.class - weight

---
+++ src/RestaurantObjects/Menu.java
@@ -0,0 +1,15 @@
+package RestaurantObjects;
+import java.util.ArrayList;
+
+import GUI.*;
+
+public class Menu {
+	public static ArrayList<MenuItem> menuList;
+	private String test;
+	public Menu() {
+		this.menuList = new ArrayList<MenuItem>();
+		menuList.add(new MenuItem("Минерална вода", price, category))
+		this.test = test;
+	}
+
+}
--- src/RestaurantObjects/MenuItem.java
+++ src/RestaurantObjects/MenuItem.java
@@ -1,12 +1,17 @@
+package RestaurantObjects;
 
 public class MenuItem {
 	private String name;
 	private double price;
 	private String category;
-	public MenuItem(String name, double price, String category) {
+	private String weight;
+	
+	
+	public MenuItem(String name, double price, String weight, String category) {
 		this.name = name;
 		this.price = price;
 		this.category = category;
+		this.weight = weight;
 	}
 	public String getName() {
 		return name;
